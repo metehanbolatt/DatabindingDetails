bc35a4bba83a00be3600d436e641b432d462fc09
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-19 02:46:09 +0300

    Include TextView in xml(layout) file with data binding

--- app/src/main/java/com/metehanbolat/databindingdetails/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/databindingdetails/MainActivity.kt
@@ -11,27 +11,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var myViewModel: MyViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        myViewModel = ViewModelProvider(this)[MyViewModel::class.java]
-
-        binding.viewModel = myViewModel
-        binding.lifecycleOwner = this
-
-        binding.editText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-            override fun onTextChanged(char: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                myViewModel.setMessage(char.toString())
-            }
-            override fun afterTextChanged(p0: Editable?) {}
-
-        })
+        val user = User("Metehan", "Bolat", 23, true)
+        binding.user = user
 
     }
 }
