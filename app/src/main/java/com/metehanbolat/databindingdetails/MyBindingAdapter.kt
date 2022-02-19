639656339a8e753d01b923b7d630309258a36337
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-19 03:03:45 +0300

    Multiple parameters with custom binding adapter

--- app/src/main/java/com/metehanbolat/databindingdetails/MyBindingAdapter.kt
+++ app/src/main/java/com/metehanbolat/databindingdetails/MyBindingAdapter.kt
@@ -1,16 +1,12 @@
 package com.metehanbolat.databindingdetails
 
+import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
 
-class MyBindingAdapter {
-
-    companion object {
-        @BindingAdapter("loadImageFromUrl")
-        @JvmStatic
-        fun ImageView.loadImageFromUrl(profilePhoto: String) {
-            this.load(profilePhoto)
-        }
-    }
+@BindingAdapter("loadImageFromUrl", "displayTitle", requireAll = true)
+fun ImageView.loadImageFromUrl(profilePhoto: String, title: String) {
+    this.load(profilePhoto)
+    Log.d("Title", title)
 }
